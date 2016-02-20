221004ce61a58a4bd3be0761f7c435a30f8aca19
Alex Forrence <aforren1@jhu.edu>
2016-02-20 15:01:30 -0500

    Added live plotting in R

---
+++ R-app/serial_example/app.R
@@ -0,0 +1,97 @@
+library(shiny)
+library(shinythemes)
+library(serial)
+library(tidyr)
+library(dplyr)
+library(ggvis)
+
+ser_conn <- serialConnection(name = "ardy", port = "ttyACM0",
+                             mode = "9600,n,8,1") # make sure this matches your arduino code
+open(ser_conn)
+
+# hacky way to try/catch; for whatever reason, I can't catch errors in external C code
+if(nchar(geterrmessage() > 1)) {
+  live <- 0
+  message('No device, going into simulation...')
+} else {
+  live <- 1
+}
+
+if(live) {
+  Sys.sleep(.1)
+  d <- read.serialConnection(ser_conn) # exclude the header of the data -- difficult to plot text!
+  Sys.sleep(.1)
+  dat <- read.delim(textConnection(read.serialConnection(ser_conn)), header = FALSE)[1:3]
+} else {
+  set.seed(1)
+  dat <- data.frame(V1 = 0, X2 = 0, X3 = 0, X4 = rnorm(1))
+  counter <- 0
+}
+
+ui <- shinyUI(fluidPage(
+  theme = shinytheme("readable"), # theme is entirely optional
+  titlePanel("Serial Port Mk 4"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      checkboxGroupInput("grps", "Channels to use:",
+                         names(dat)[-1], selected = names(dat[-1])), # all columns except first
+      sliderInput("nsamples", "Samples into the past:",
+                  min = 10, max = 200, value = 80),
+      sliderInput("yaxis", "Y-axis limits:", -10, 10, c(-3, 3)),
+      downloadButton('downloadData', 'Download')
+    ),
+
+    # show plot in the main panel    
+    mainPanel(
+      uiOutput("ggvis_ui"),
+      ggvisOutput("ggvis")
+    )
+  )
+))
+
+server <- shinyServer(function(input, output){
+  
+  fetchData <- reactive({
+    invalidateLater(1, NULL)
+    if(live){ # getting data from the serial port
+      dat <<- rbind(dat, 
+                    read.delim(textConnection(read.serialConnection(ser_conn)),
+                               header = FALSE)[1:3])
+    } else { # generating more fake data
+      counter <<- counter + 1
+      dat <<- rbind(dat, data.frame(V1 = 0.002 * counter, X2 = ifelse(counter %% 5 == 0, 2, -2),
+                                    X3 = ifelse(counter %% 7 == 0, -1, 1), X4 = rnorm(1)))
+    }
+    gather(dat, group, value, -V1) # from tidyr
+  })
+  
+  fetchData %>% 
+    group_by(group) %>% 
+    arrange(V1) %>% # sort by time/sample number
+    filter(row_number() >= n() - input$nsamples) %>% # display the `nsamples` most current samples
+    filter(group %in% input$grps) %>% 
+    ggvis(~V1, ~value, stroke = ~group) %>% 
+    layer_lines() %>% 
+    scale_numeric("y", domain = reactive(input$yaxis), nice = TRUE, clamp = TRUE) %>%  
+    set_options(duration = 0.1) %>% # 0 makes the legend jump around, > 1 leads to weird transition effects
+    bind_shiny("ggvis", "ggvis_ui")
+  
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste('data-', Sys.Date(), '.csv', sep='')
+    },
+    content = function(file) {
+      write.csv(dat, file)
+    }
+  )
+  # cancel.onSessionEnded <- session$onSessionEnded(function() {
+  #   close(ser_conn)
+  #   disp('Closed serial connection')
+  # })
+})
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
